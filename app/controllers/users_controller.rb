ef5b23a162161c16cefee85db4be1e52a7083cc7
Radhakrishna <radhac88@gmail.com>
2014-05-20 12:38:56 +0530

    Simple authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+  class UsersController < ApplicationController
+
+  before_filter :save_login_state, :only => [:new, :create]
+
+  def new
+      #Signup Form
+      @user = User.new     
+  end
+
+  def create
+    	@user = User.new(params[:user])
+    	if @user.save
+    		flash[:notice] = "You Signed up successfully"
+        flash[:color]= "valid"
+      else
+        flash[:notice] = "Form is invalid"
+        flash[:color]= "invalid"
+      end
+      render "new"
+  end
+
+  def index
+      @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end 
+  def show
+     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
